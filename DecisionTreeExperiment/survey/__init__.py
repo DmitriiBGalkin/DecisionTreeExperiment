a3718025d61534869a2267cf2394abe19db05537
DmitriiBGalkin <d.b.galkin@gmail.com>
2025-03-12 12:32:22 +0100

    Updates

--- DecisionTreeExperiment/survey/__init__.py
+++ DecisionTreeExperiment/survey/__init__.py
@@ -1,5 +1,6 @@
 from otree.api import *
 from settings import LANGUAGE_CODE, SESSION_CONFIGS
+
 if LANGUAGE_CODE == 'de':
     from .lexicon_de import Lexicon
 else:
@@ -23,115 +24,128 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-        familiarity_with_decision_trees = models.IntegerField(
-            choices=[
-                (1, Lexicon.familiarity_with_decision_trees_1),
-                (2, Lexicon.familiarity_with_decision_trees_2),
-                (3, Lexicon.familiarity_with_decision_trees_3),
-                (4, Lexicon.familiarity_with_decision_trees_4),
-                (5, Lexicon.familiarity_with_decision_trees_5),
-            ],
-            label=Lexicon.familiarity_with_decision_trees_label,
-            widget=widgets.RadioSelectHorizontal,
-        )
-        sample_question_1 = models.BooleanField()
-        sample_question_2 = models.BooleanField()
-        #Survey Questions
-        gender = models.IntegerField(
-            choices=[
-                (1, Lexicon.gender_male),
-                (2, Lexicon.gender_female),
-                (3, Lexicon.gender_diverse),
-                (4, Lexicon.gender_no_answer),
-            ],
-            label=Lexicon.gender_label,
-            widget=widgets.RadioSelect,
-        )
-        age = models.IntegerField(
-            label=Lexicon.age_label,
-            min=18,
-            max=100,
-            error_message=Lexicon.age_error_message,
+    familiarity_with_decision_trees = models.IntegerField(
+        choices=[
+            (1, Lexicon.familiarity_with_decision_trees_1),
+            (2, Lexicon.familiarity_with_decision_trees_2),
+            (3, Lexicon.familiarity_with_decision_trees_3),
+            (4, Lexicon.familiarity_with_decision_trees_4),
+            (5, Lexicon.familiarity_with_decision_trees_5),
+        ],
+        label=Lexicon.familiarity_with_decision_trees_label,
+        widget=widgets.RadioSelectHorizontal,
+    )
+    sample_question_1 = models.BooleanField()
+    sample_question_2 = models.BooleanField()
+    # Survey Questions
+    gender = models.IntegerField(
+        choices=[
+            (1, Lexicon.gender_male),
+            (2, Lexicon.gender_female),
+            (3, Lexicon.gender_diverse),
+            (4, Lexicon.gender_no_answer),
+        ],
+        label=Lexicon.gender_label,
+        widget=widgets.RadioSelect,
+    )
+    age = models.IntegerField(
+        label=Lexicon.age_label,
+        min=18,
+        max=100
+    )
+    education_level = models.StringField(
+        choices=[
+            Lexicon.education_schueler,
+            Lexicon.education_hauptschule,
+            Lexicon.education_mittlere_reife,
+            Lexicon.education_lehre,
+            Lexicon.education_fachabitur,
+            Lexicon.education_abitur,
+            Lexicon.education_hochschulabschluss,
+            Lexicon.education_other,
+        ],
+        label=Lexicon.education_label
+    )
+    education_level_other = models.StringField(
+        label="Falls Sie 'Anderer Abschluss' gewählt haben, bitte angeben:",
+        blank=True,  # This allows the field to remain empty
+    )
+    serious_participation = models.BooleanField(
+        choices=[
+            (1, Lexicon.participation_serious),
+            (2, Lexicon.participation_not_serious),
+        ],
+        label=Lexicon.participation_label
+    )
+    feedback = models.LongStringField(
+        label=Lexicon.feedback_label,
+        blank=True,
+    )
+    bundesland = models.StringField(
+        choices=[
+            (1, "Baden-Württemberg"),
+            (2, "Bayern"),
+            (3, "Berlin"),
+            (4, "Brandenburg"),
+            (5, "Bremen"),
+            (6, "Hamburg"),
+            (7, "Hessen"),
+            (8, "Mecklenburg-Vorpommern"),
+            (9, "Niedersachsen"),
+            (10, "Nordrhein-Westfalen"),
+            (11, "Rheinland-Pfalz"),
+            (12, "Saarland"),
+            (13, "Sachsen"),
+            (14, "Sachsen-Anhalt"),
+            (15, "Schleswig-Holstein"),
+            (16, "Thüringen"),
+        ],
+        label=Lexicon.state_label,
+    )
 
-        )
-        education_level = models.StringField(
-            choices=[
-                (Lexicon.education_schueler),
-                (Lexicon.education_hauptschule),
-                (Lexicon.education_mittlere_reife),
-                (Lexicon.education_lehre),
-                (Lexicon.education_fachabitur),
-                (Lexicon.education_abitur),
-                (Lexicon.education_hochschulabschluss),
-                (Lexicon.education_other),
-            ],
-            label=Lexicon.education_label
-        )
-        education_level_other = models.StringField(
-            label="Falls Sie 'Anderer Abschluss' gewählt haben, bitte angeben:",
-            blank=True,  # This allows the field to remain empty
-        )
-        serious_participation = models.BooleanField(
-            choices=[
-                (1, Lexicon.participation_serious),
-                (2, Lexicon.participation_not_serious),
-            ],
-            label=Lexicon.participation_label
-        )
-        feedback = models.LongStringField(
-            label=Lexicon.feedback_label,
-            blank=True,
-        )
-        bundesland = models.StringField(
-            choices=[
-                (1, "Baden-Württemberg"),
-                (2, "Bayern"),
-                (3, "Berlin"),
-                (4, "Brandenburg"),
-                (5, "Bremen"),
-                (6, "Hamburg"),
-                (7, "Hessen"),
-                (8, "Mecklenburg-Vorpommern"),
-                (9, "Niedersachsen"),
-                (10, "Nordrhein-Westfalen"),
-                (11, "Rheinland-Pfalz"),
-                (12, "Saarland"),
-                (13, "Sachsen"),
-                (14, "Sachsen-Anhalt"),
-                (15, "Schleswig-Holstein"),
-                (16, "Thüringen"),
-            ],
-            label=Lexicon.state_label,
-        )
 
 # FUNCTIONS
 # PAGES
 class IntroductionGeneral(Page):
+    @staticmethod
     def vars_for_template(player: Player):
         return dict(
             Lexicon=Lexicon,
             **which_language)
+
 
 class IntroductionDecisionTrees(Page):
     form_model = 'player'
     form_fields = ['familiarity_with_decision_trees']
-    def vars_for_template(player: Player):
-        return dict(
-            Lexicon=Lexicon,
-            **which_language)
-class InstructionsSample(Page):
-    def vars_for_template(player: Player):
-        return dict(
-            Lexicon=Lexicon,
-            **which_language)
-class Survey(Page):
+
+    @staticmethod
     def vars_for_template(player: Player):
         return dict(
             Lexicon=Lexicon,
             **which_language)
 
+
+class InstructionsSample(Page):
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            Lexicon=Lexicon,
+            **which_language)
+
+class SampleQuestion(Page):
+    pass
+
+class Survey(Page):
+    @staticmethod
+    def vars_for_template(player: Player):
+        return dict(
+            Lexicon=Lexicon,
+            **which_language)
     form_model = 'player'
-    form_fields= ['gender','age','education_level','education_level_other','bundesland','serious_participation','feedback']
+    form_fields = ['gender', 'age', 'education_level', 'education_level_other', 'bundesland', 'serious_participation',
+                   'feedback']
+
+
 class CognitiveReflectionTest(Page):
     form_model = 'player'
     form_fields = ['crt_bat', 'crt_widget', 'crt_lake']
